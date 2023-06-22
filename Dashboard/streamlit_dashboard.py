4254979c43c2997f54964a46adad380bdbfec5d7
Haelios <133202042+Haelios@users.noreply.github.com>
2023-06-22 02:41:22 +0200

    V1.2
    Ajout des différentes fonctionnalités sur le Dashboard :
    - Menu à cliquer sur le côté permettant l'affichage des différentes pages
    - Modification de la jauge afin de la rendre plus facilement lisible et compréhensible directement
    - Modification du number_input afin de conserver l'id entré, et d'attendre que le client entre une valeur avant de lancer la suite.
    - Affichage d'un force plot local pour expliquer au client les points qui posent le plus problème dans son dossier
    - Affichage de bar plots permettant de comparer les résultats du client par rapport à la moyenne des prêts acceptés sur les variables importantes du modèle
    - Affichage d'un scatterplot afin d'observer les relations entre les ext source (Mauvais pour l'instant, à modifier ou retirer)
    - Modification de fond du code afin d'assurer le bon déroulement de celui ci et améliorer l'expérience utilisateur.

--- Dashboard/streamlit_dashboard.py
+++ Dashboard/streamlit_dashboard.py
@@ -1,44 +1,152 @@
 import streamlit as st
+import streamlit_extras.altex
+from streamlit_shap import st_shap
+from streamlit_extras.altex import bar_chart, scatter_chart, line_chart, chart
 import requests
 import time
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from numpy import radians, cos, sin
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
+import shap
 
 # API endpoint URL
-API_URL = 'http://localhost:5000/loan_prediction'
+API_URL = 'http://localhost:5000/prediction'
 
 
-def main() :
+# client_id = 100002  # (Pour ajouter des infos et debug, sans avoir à entrer d'ID à chaque fois)
+
+options = st.sidebar.radio('Détails', options=['Accueil', 'Feature Importance', 'Comparaisons', 'Analyse Bivariée'])
+
+
+def get_response():
     st.title('Dashboard demande de prêt')
-
-    id_widget = st.sidebar.empty()
-    client_id = id_widget.text_input("Enter your client ID")
-
-    if client_id != '':
+    id_widget = st.empty()
+    id_client = id_widget.number_input("Enter your client ID")
+    if id_client :
         id_widget.empty()
-        st.sidebar.write(client_id)
+        resp = requests.post(API_URL, json={'loan_id': id_client})
+        return resp, id_client
+    else:
+        st.stop()
 
-    if client_id:
-        # Make a request to the API
-        response = requests.post(API_URL, json={'loan_id': client_id})
 
-        if response.status_code == 200:
-            if len(response.text)==0:
-                return "La requête a échoué, veuillez vérifier votre saisie."
-
-            prediction = response.json()
-
-            # Display loan prediction
-            st.subheader('Loan Prediction')
-            st.write(prediction)
-
-            ## Display client data
-            #st.subheader('Client Data')
-            #client_data = pd.DataFrame(data['CODE_GENDER'])
-            #st.write(client_data)
+def accueil():
+    """
+    Page d'accueil du Dashboard, affiche le résultat de la demande de prêt, avec une jauge affichant le score (proba)
+    du client et le seuil utilisé pour la prédiction.
+    """
+    # Vérifier que la requête a bien fonctionné
+    if st.session_state.response.status_code == 200:
+        # Vérifier que la réponse contient des données
+        if len(st.session_state.response.text) == 0:
+            return "La requête a échoué, veuillez vérifier votre saisie."
         else:
-            st.error('Error retrieving loan prediction. Please try again.')
-            st.text(response.status_code)
+            index = st.session_state.response.json()['index']
+            client_prediction = st.session_state.response.json()['prediction'][index]
+
+            # Afficher le résultat de la demande
+            st.subheader('Loan Prediction')
+            if client_prediction == 0:
+                st.write('Votre prêt a été accepté !')
+            else:
+                st.write("Votre demande de prêt n'a pas été acceptée.")
+
+            # Afficher la jauge avec le score du client
+            score = st.session_state.response.json()['score']
+
+            fig = go.Figure(go.Indicator(
+                            mode = "gauge+number",
+                            value = (1-score)*100,
+                            domain = {'x': [0, 1], 'y': [0, 1]},
+                            title = {'text': "Score prêt", 'font': {'size': 24}},
+                            gauge = {
+                                'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "white"},
+                                'bar': {'color': "black", 'thickness': 1},
+                                'borderwidth': 2,
+                                'bordercolor': "black",
+                                'threshold': {
+                                    'line': {'color': "red", 'width': 3},
+                                    'thickness': 1,
+                                    'value': 50}}))
+
+            st.plotly_chart(fig)
+    else:
+        st.error('Error retrieving loan prediction. Please try again.')
+        st.text(st.session_state.response.status_code)
 
 
-if __name__ == '__main__':
-    main()
+def feature_imp():
+    all_data = pd.DataFrame(st.session_state.response.json()['all_client_val'], columns=st.session_state.response.json()['all_client_col'])
+    shap_values = st.session_state.response.json()['shap_val']
+    expected_value = st.session_state.response.json()['expected_val']
+    fig2 = shap.force_plot(expected_value, np.array(shap_values), all_data, feature_names=all_data.columns)
+    st_shap(fig2, width=900)
+
+
+def comparison_graphs():
+    ext_values = st.session_state.response.json()['comp_graphs'][0]
+    ext_feature = ['Ext1', 'Ext2', 'Ext3', 'Ext1', 'Ext2', 'Ext3']
+    ext_clients = ['client', 'client', 'client', 'mean', 'mean', 'mean']
+    df_ext = pd.DataFrame([ext_values, ext_feature, ext_clients], index= ['values', 'feature', 'clients']).T
+
+    bar_chart(
+        data=df_ext,
+        x="clients:N",
+        y="values:Q",
+        color="clients:N",
+        column="feature:N",
+        title="Comparaison score client et moyenne des prêts validés : External Sources",
+        width=150,
+        use_container_width=False,
+    )
+
+    days_values = st.session_state.response.json()['comp_graphs'][1]
+    days_feat = ['Days Employed']
+    days_clients = ['client', 'mean']
+    df_days = pd.DataFrame([days_values, days_feat, days_clients], index=['values', 'feature', 'clients']).T
+
+    bar_chart(
+        data=df_days,
+        x="clients",
+        y="values",
+        title="Comparaison score client et moyenne des prêts validés : Days Employed",
+        width=500,
+        height=500,
+        color = "clients"
+    )
+
+
+def bivar_graphs():
+    ext1 = st.session_state.response.json()['bivar_graphs'][0]
+    ext2 = st.session_state.response.json()['bivar_graphs'][1]
+    ext3 = st.session_state.response.json()['bivar_graphs'][2]
+    target = st.session_state.response.json()['prediction']
+    df_bivar = pd.DataFrame(zip(ext1, ext2, ext3, target), columns = ['Ext1', 'Ext2', 'Ext3', 'Prediction'])
+
+    scatter_chart(
+        data = df_bivar,
+        x="Ext1",
+        y="Ext2",
+        color="Prediction:N",
+        title="A beautiful scatter chart",
+        height = 1000,
+        width = 1000
+    )
+
+
+if 'response' not in st.session_state:
+    st.session_state.response, st.session_state.client_id = get_response()
+if 'client_id' in st.session_state:
+    st.sidebar.write(st.session_state.client_id)
+    if options == 'Accueil':
+        accueil()
+    elif options == 'Feature Importance':
+        feature_imp()
+    elif options == 'Comparaisons':
+        comparison_graphs()
+    elif options == 'Analyse Bivariée':
+        bivar_graphs()
